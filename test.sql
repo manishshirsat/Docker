6eb64c124359f894d1a42031633924552c7522f2
Manish Shirsat <87015624+manishshirsat@users.noreply.github.com>
2022-01-08 22:10:46 +0530

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 07, 2022 at 06:51 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pucsdstudents`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentdata`
+--
+
+CREATE TABLE `studentdata` (
+  `Name` varchar(50) NOT NULL,
+  `Roll no` int(10) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Mobile Number` bigint(12) NOT NULL,
+  `PAN Number` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `studentdata`
+--
+
+INSERT INTO `studentdata` (`Name`, `Roll no`, `Address`, `Mobile Number`, `PAN Number`) VALUES
+('Will Jacks', 1255, 'Sydney', 7899554412, 'ML782WP156');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
